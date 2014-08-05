dab189740152b428d2318f7dc7ff770996fe2185
Fábio Matavelli <fabiomatavelli@gmail.com>
2014-08-05 13:05:06 -0300

    Implementado serviço Loads
    Implementado serviço Loads para realizar carga dos produtos do lojista.

--- src/Extra.py
+++ src/Extra.py
@@ -1,9 +1,11 @@
-#!/usr/env python
+﻿#!/usr/env python
 # coding=utf-8
 __author__ = "Fábio Matavelli <fabiomatavelli@gmail.com>"
 
 import requests
 import json
+import gzip
+from cStringIO import StringIO
 
 class API:
 	# Ambiente Sandbox
@@ -60,13 +62,13 @@ class API:
 		return cls.Call(Method="get",Path=Path,**kwargs)
 	
 	@classmethod
-	def post(cls,Path,**kwargs):
+	def post(cls,Path,CType=1,**kwargs):
 		"""Método post da API
 		
 		Args:
 			Path: Caminho da requisição
 		"""
-		return cls.Call(Method="post",Path=Path,**kwargs)
+		return cls.Call(Method="post",Path=Path,CType=CType,**kwargs)
 		
 	@classmethod
 	def put(cls,Path,**kwargs):
@@ -309,6 +311,128 @@ class SellerItems(API):
 			_limit: Limita a quantidade de registros trazidos pela consulta
 		"""
 		return cls.get("/sellerItems/status/selling",**kwargs)
+
+class Loads(API):
+	# Armazenda os produtos utilizando o AddProduct
+	__products = []
+	
+	@classmethod
+	def AddProduct(cls,skuIdOrigin,sellingTitle,description,brand,defaultPrice,salePrice,categoryList,Weight,Length,Width,Height,availableQuantity,handlingTime,images,videos=[],productUdaLists=[],skuId=None,productIdOrigin=None,EAN=None,installmentId=None):
+		"""Adiciona um produto à variável __products para ser utilizada pelo método Load.
 		
+		Args:
+			skuIdOrigin (string): SKU ID do produto do lojista,
+			skuId (string, optional): SKU ID do produto no Marketplace - utilizado para realizar associação de produtos,
+			productIdOrigin (string, optional): ID do produto do lojista para fazer o agrupamento de SKUs,
+			sellingTitle (string): Nome no Marketplace. Ex Televisor de LCD Sony Bravia 40”...,
+			description (string): Descrição do produto. Aceita tags HTML para formatar a apresentação da descrição, no entanto há alguma
+			tags restritas (tags para acesso a conteúdo externo):img, object, script e iframe.,
+			brand (string): Marca. Ex Sony,
+			EAN (string, optional): Código EAN do produto (código de barras),
+			defaultPrice (double): Preço “de” no Marketplace. Não pode ser 0 (zero) e deve ter no máximo duas casas decimais,
+			salePrice (double): Preço “por”. Preço real de venda. Não pode ser 0 (zero) e deve ter no máximo duas casas decimais,
+			categoryList (List[string]): Lista de categorias. Quantidade máxima de cinco (5) níveis de categoria por produto.,
+			productUdaLists (List[productUdas]): Lista de atributos do Produto. Quantidade máxima de 38 atributos por produto.,
+			Weight (double): Peso do produto, em quilos. Não pode ser 0 (zero) e deve ter no máximo duas casas decimais,
+			Length (double): Comprimento do produto, em metros. Não pode ser 0 (zero) e deve ter no máximo duas casas decimais,
+			Width (double): Largura do produto, em metros. Não pode ser 0 (zero) e deve ter no máximo duas casas decimais,
+			Height (double): Altura do produto, em metros. Não pode ser 0 (zero) e deve ter no máximo duas casas decimais,
+			availableQuantity (int): Quantidade disponível,
+			handlingTime (int): Tempo de fabricação em dias (padrão é 0),
+			installmentId (int, optional): ID do parcelamento,
+			videos (List[string], optional): Lista de URLs de vídeos. Quantidade máxima de dois (2) vídeos por produto.,
+			images (List[string]): Lista de URLs de imagens. Pelo menos uma imagem precisa ser informada. Quantidade máxima de quatro (4) imagens por produto.
+		"""
+		for product in cls.__products:
+			if product["skuIdOrigin"] == skuIdOrigin:
+				raise Exception("Produto '%s' ja existe na lista de produtos para carga." % (skuIdOrigin,))
+		
+		cls.__products.append({
+			"skuIdOrigin":skuIdOrigin,
+			"skuId":skuId,
+			"productIdOrigin":productIdOrigin,
+			"sellingTitle":sellingTitle,
+			"description":description,
+			"brand":brand,
+			"EAN":EAN,
+			"defaultPrice":defaultPrice,
+			"salePrice":salePrice,
+			"categoryList":categoryList,
+			"productUdaLists":productUdaLists,
+			"Weight":Weight,
+			"Length":Length,
+			"Width":Width,
+			"Height":Height,
+			"availableQuantity":availableQuantity,
+			"handlingTime":handlingTime,
+			"installmentId":installmentId,
+			"videos":videos,
+			"images":images
+			})
+			
+		return cls.__products
+	
+	@classmethod
+	def ClearProducts(cls):
+		"""Limpa a lista de produtos __products
+		"""
+		cls.__products = []
+	
+	@classmethod
+	def DelProduct(cls,skuIdOrigin):
+		"""Deleta um produto da lista de acordo com o skuIdOrigin
+		"""
+		for key,product in enumerate(cls.__products):
+			if product["skuIdOrigin"] == skuIdOrigin:
+				del(cls.__products[key])
+		else:
+			raise Exception(u"Produto '%s' não encontrado." % (skuIdOrigin,))
+			
+		return cls.__products
+		
+	@classmethod
+	def GetProducts(cls):
+		"""Retorna a lista de produtos
+		
+		Return:
+			Retorna a lista de produtos __products
+		"""
+		
+		return cls.__products
+	
+	@classmethod
+	def Load(cls,**kwargs):
+		"""Realiza a carga dos produtos armazenados no __products
+		
+		Return:
+			Retorna o ID da carga
+		"""
+		
+		# Compacta o json do __products
+		stream = StringIO()
+		with gzip.GzipFile(fileobj=stream,mode="wb") as compress:
+			compress.write(json.dumps(cls.__products))
+		
+		r = cls.post("/loads/products",data=stream.getvalue(),CType=2,**kwargs)
+		if r.status_code == 201:
+			l = r.headers["location"].split("/")
+			return l[-1]
+		else:
+			error = r.json()
+			raise Exception(error["errorDesc"],error["errorCode"])
+	
+	@classmethod
+	def ImporterInfo(cls,importerInfoId,**kwargs):
+		"""Consulta o status da carga dos produtos.
+		"""
+		
+		return cls.get("/loads/products/%s" % (importerInfoId,),**kwargs)
+	
+	@classmethod
+	def ImporterInfoSku(cls,importerInfoId,skuOrigin,**kwargs):
+		"""Consulta a informação de um produto carregado através da operação de importação
+		"""
+		return cls.get("/loads/products/%s/%s" % (importerInfoId,skuOrigin),**kwargs)
+	
 if __name__ == "__main__":
 	pass
